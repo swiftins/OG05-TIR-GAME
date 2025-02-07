eec152b666afeeea2a1f406b974c582aa761c21c
АСУС <natalja.rejenta@gmail.com>
2025-02-07 13:15:48 +0200

    add circle

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("ИГРА ТИР")
+pygame.display.set_caption("ИГРА ТИР-АМУР")
 
 # Установка иконки окна
 icon = pygame.image.load("target.jpg")
